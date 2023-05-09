666e38167e6511912eb740ef179b56e45fecb492
IlhamPratama <pratamailham206@gmail.com>
2023-05-09 13:33:24 +0700

    Initial Project

---
+++ Models/StandardPage.cs
@@ -0,0 +1,9 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace dotnet_piranha_postgresql.Models;
+
+[PageType(Title = "Standard page")]
+public class StandardPage  : Page<StandardPage>
+{
+}
